902772b0d6ed73002d48dddc321187e128e9350d
Yura Quick <quickyura@gmail.com>
2021-05-12 16:24:58 +0300

    'initial'

---
+++ middleware/middleware_auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config =  require('config');
+module.exports=(req,res,next)=>{
+
+    if (req.method === 'OPTIONS') {
+        return next()
+    }
+
+    try {
+
+        const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+
+        if (!token) {
+            return res.status(401).json({ message: 'Нет авторизации' })
+        }
+
+       req.user = jwt.decode(token,config.get('jwt') );
+
+
+        next()
+    }catch (e) {
+
+        res.status(401).json({massage:"нет авторизации"})
+    }
+};
